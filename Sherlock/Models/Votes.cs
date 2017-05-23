22d5e3bdd80ae729bced0dc80b422b0791745692
Pranay Maharana <pranay.maharana@gmail.com>
2017-05-23 12:48:21 -0400

    --added tables

--- Sherlock/Models/Votes.cs
+++ Sherlock/Models/Votes.cs
@@ -8,5 +8,16 @@ namespace Sherlock.Models
     public class Votes
     {
         public int Id { get; set; }
+        public int Value { get; set; }
+
+        public DateTime DateCasted { get; set; } = DateTime.Now;
+
+        public int LandmarkId { get; set; }
+        public Landmark Landmark { get; set; }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+
     }
 }
