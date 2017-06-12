0eb42403641e255fab25829c86438b8b71c181c9
Pranay Maharana <pranay.maharana@gmail.com>
2017-06-12 19:20:28 -0400

    --geolocation onclick works

--- Sherlock/Scripts/map.js
+++ Sherlock/Scripts/map.js
@@ -6,7 +6,9 @@ let newmarker;
 
 
 
+
 function initMap() {
+
     let uluru = { lat: 27.771344, lng: -82.635745 };
     mappy = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
@@ -21,10 +23,44 @@ function initMap() {
             { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": 
                 [{ "color": "#ae9e90" }] }, { "featureType": "landscape.natural", "elementType": "geometry", "stylers": [{ "color": "#dfd2ae" }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#dfd2ae" }] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [{ "color": "#93817c" }] }, { "featureType": "poi.business", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [{ "color": "#a5b076" }] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [{ "color": "#447530" }] }, { "featureType": "road", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road", "elementType": "geometry", "stylers": [{ "color": "#f5f1e6" }] }, { "featureType": "road", "elementType": "labels", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#fdfcf8" }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#f8c967" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#e9bc62" }] }, { "featureType": "road.highway.controlled_access", "elementType": "geometry", "stylers": [{ "color": "#e98d58" }] }, { "featureType": "road.highway.controlled_access", "elementType": "geometry.stroke", "stylers": [{ "color": "#db8555" }] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [{ "color": "#806b63" }] }, { "featureType": "transit", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [{ "color": "#dfd2ae" }] }, { "featureType": "transit.line", "elementType": "labels.text.fill", "stylers": [{ "color": "#8f7d77" }] }, { "featureType": "transit.line", "elementType": "labels.text.stroke", "stylers": [{ "color": "#ebe3cd" }] }, { "featureType": "transit.station", "elementType": "geometry", "stylers": [{ "color": "#dfd2ae" }] }, { "featureType": "water", "elementType": "geometry.fill", "stylers": [{ "color": "#b9d3c2" }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "color": "#92998d" }] }]
     });
+
     talkToServer();
+
 }
 
 
+   
+     
+
+
+
+function geoMap() {
+    let mapuu = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 27.771344, lng: -82.635745 },
+        zoom: 15
+    });
+    let infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(mapuu);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
 
 
 function initMapAuth() {
@@ -115,12 +151,11 @@ let talkToServer = () => {
                     '<div id="bodytent">' + '<img src="' + item.Image1 + '" alt="' + item.Title +'" />' +
                     '<p>' + item.Description + '</p>' +
                     //'<a href= "' + item.Links + '" target="_blank"> More Info...</a>' +
+                    '<a href= "/Home/LandmarkComments?id=' + item.Id + '" > More Info...</a>' +
                     '</div>' +
                     '</div>'
 
-                    //item.Title +
-                    //'<img src="' + item.Image1 + '"></img>' +
-                    //item.Description
+                  
                     ;
 
                 let infowindow = new google.maps.InfoWindow({
@@ -230,29 +265,6 @@ function saveComment() {
 
 
 
-// GeoLocation
-//var elMap = document.getElementById('loc');                 // HTML element
-//var msg = 'Sorry, we were unable to get your location.';    // No location msg
-
-//if (Modernizr.geolocation) {                                // Is geo supported
-//    navigator.geolocation.getCurrentPosition(success, fail);  // Ask for location
-//    elMap.textContent = 'Checking location...';               // Say checking...
-//} else {                                                    // Not supported
-//    elMap.textContent = msg;                                  // Add manual entry
-//}
-
-//function success(position) {                                // Got location
-//    msg = '<h3>Longitude:<br>';                               // Create message
-//    msg += position.coords.longitude + '</h3>';               // Add longitude
-//    msg += '<h3>Latitude:<br>';                               // Create message
-//    msg += position.coords.latitude + '</h3>';                // Add latitude
-//    elMap.innerHTML = msg;                                    // Show location
-//}
-
-//function fail(msg) {                                        // Not got location
-//    elMap.textContent = msg;                                  // Show text input
-//    console.log(msg.code);                                    // Log the error
-//}
 
 
 
